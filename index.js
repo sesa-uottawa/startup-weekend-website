bac299c3721c0e7ffe23ad61397ded56689bd519
Alex Kluew <dropbyalex@gmail.com>
2016-08-23 14:40:01 -0400

    Forgot to adjust for custom ports

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/', express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port: ' + app.get('port'));
 });
