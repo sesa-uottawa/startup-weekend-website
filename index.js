fafc029764cc11f3b343aac197ee3b17edac1c19
Alex Kluew <dropbyalex@gmail.com>
2016-09-08 13:30:21 -0400

    Removing v2 link

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.get('/v2', function(req,res){
- res.sendfile(__dirname + '/public/index-3.html');
-});
+// app.get('/v2', function(req,res){
+//  res.sendfile(__dirname + '/public/index-3.html');
+// });
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port: ' + app.get('port'));
